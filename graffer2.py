5dc9c70f8cea0551f8ac04d458fa57d99b125bf8
David Konerding <dakoner@gmail.comwq>
2024-03-29 15:24:10 -0700

    trying out my own list model

--- graffer2.py
+++ graffer2.py
@@ -25,54 +25,61 @@ class Node(QtWidgets.QGraphicsEllipseItem):
         #self.setBrush(QtGui.QGradient(QtGui.QGradient.SaintPetersburg))
         self.setFlags(QtWidgets.QGraphicsItem.ItemSendsGeometryChanges | QtWidgets.QGraphicsItem.ItemIsMovable)
 
-class GraphScene(QtWidgets.QGraphicsScene):
-    def inserted(self, obj, first, last):
-        print("inserted", obj, first, last)
-        print(self.model.index(first, 0).data())
+class GraphNodeListModel(QtCore.QAbstractListModel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.G = nx.Graph()
 
+    def data(self, index, role):
+        if role == QtCore.Qt.DisplayRole:
+            t = list(self.G.nodes())[index.row()]
+            return t.name
+        
+    def rowCount(self, index):
+        return len(self.G.nodes)
+    
+class GraphScene(QtWidgets.QGraphicsScene):
     def __init__(self, listView, *args, **kwargs):
         self.listView = listView
-        self.model = QtGui.QStandardItemModel()
-        self.model.rowsInserted.connect(self.inserted)
+        self.model = GraphNodeListModel()
         self.listView.setModel(self.model)
 
         super().__init__(*args, **kwargs)
 
-        self.G = nx.Graph()
 
-
-        #print(dir(QtWidgets.QApplication.instance()))
-        #self.listView = QtWidgets.QApplication.instance().main_window.findChild(QtWidgets.QListView, "listView")
-        #print(self.listView)
     def addGraph(self, g, pos):
 
+        nodemap = {}
         for node in g.nodes():
             item = Node(0, 0, .01, .01)
-            self.addItem(item)
-            n = QtGui.QStandardItem(f"Node {node}")
-            n.setData("foo")
-            self.model.appendRow(n)
-            item.node = node
+            item.name = f"Node {node}"
             item.setPos(*pos[node])
-        print(self.model.index(0, 0).data())
+            self.addItem(item)
+            nodemap[node] = item            
+            self.model.G.add_node(item)
+
         for edge in g.edges():
             p0 = pos[edge[0]]
             p1 = pos[edge[1]]
             item = Edge(p0[0]+.025, p0[1]+.025, p1[0]+.025, p1[1]+.025)
             self.addItem(item)
 
+            self.model.G.add_edge(nodemap[edge[0]], nodemap[edge[1]])
+
+        self.model.rowsInserted.emit(QtCore.QModelIndex(), 0, len(g.nodes()))
 
     def mousePressEvent(self, event):
         print("scene mouse press event", event)
         item = self.itemAt(event.scenePos(), QtGui.QTransform())
         if item is None:
             item = Node(0, 0, .01, .01)
-            self.addItem(item)
-            i = max(self.G.nodes)+1
-            self.G.add_node(i)
-            self.model.appendRow(QtGui.QStandardItem(str(i)))
-            item.node = i
             item.setPos(event.scenePos())
+            self.addItem(item)
+
+            self.model.G.add_node(item)
+            l = len(self.model.G.nodes())-1
+            item.name = f"Node {l}"
+            self.model.rowsInserted.emit(QtCore.QModelIndex(), l, l)
             event.accept()
         else:
             event.ignore()
@@ -109,7 +116,9 @@ class QApplication(QtWidgets.QApplication):
         self.main_window = MainWindow()
         self.main_window.show()
 
-        G = nx.balanced_tree(2, 3)   
+        G = nx.balanced_tree(2, 3)
+        #G = nx.Graph()
+        #G.add_node(1)   
         pos = nx.spring_layout(G)
         self.main_window.graphicsView.scene().addGraph(G, pos)
 
