321319978b190f896e9329e9e5188f72936ce1d1
David Konerding <dakoner@gmail.comwq>
2024-03-29 17:05:43 -0700

    clean up graffer2ui, add test_ui.ui and improve test_ui

--- test_ui.py
+++ test_ui.py
@@ -7,6 +7,7 @@ from PyQt5 import QtWidgets
 from PyQt5 import QtCore
 from PyQt5 import QtGui
 from PyQt5.uic import loadUi
+import networkx as nx
 import signal
 import sys
 
@@ -30,18 +31,18 @@ class Node(QtWidgets.QGraphicsEllipseItem):
 class GraphNodeListModel(QtCore.QAbstractListModel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.nodes = []
+        self.graph = nx.Graph()
 
     def data(self, index, role):
         if role == QtCore.Qt.DisplayRole:
-            t = self.nodes[index.row()]
+            t = list(self.graph.nodes())[index.row()]
             return str(t)
         
     def add_node(self, node):
-        self.nodes.append(node)
+        self.graph.add_node(node)
 
     def __len__(self):
-        return len(self.nodes)
+        return len(self.graph.nodes())
     
     def rowCount(self, index):
         return len(self)
@@ -55,32 +56,43 @@ class GraphScene(QtWidgets.QGraphicsScene):
 
         super().__init__(*args, **kwargs)
 
+        item = Node("foo", 0, 0, 10, 10)
+        item.setPos(50, 50)
+        self.addItem(item)
+        self.model.add_node(item)
+        self.model.rowsInserted.emit(QtCore.QModelIndex(), 0, 0)
+        self.graphicsView.fitInView(self.sceneRect(), QtCore.Qt.KeepAspectRatio)
 
-
-    # def mouseMoveEvent(self, event):
-    #     print("scene mouse move event", event.scenePos())
-    #     return super().mouseMoveEvent(event)
+    def mouseMoveEvent(self, event):
+        print("scene mouse move event", event.scenePos())
+        return super().mouseMoveEvent(event)
 
     def mousePressEvent(self, event):
-        print("scene mouse press event", event)
         item = self.itemAt(event.scenePos(), QtGui.QTransform())
-        print(item)
-        if item is None:
+        # if there's no node at this point, and we're pressing control, add a new node
+        if item is None and event.modifiers() & QtCore.Qt.ControlModifier:
             l = len(self.model)
-            name = f"Node"
+            name = f"Node {l}"
             print("Add new", name)
             item = Node(name, 0, 0, 10, 10)
-            print(event.scenePos())
             item.setPos(event.scenePos())
             self.addItem(item)
             self.model.add_node(item)
             self.model.rowsInserted.emit(QtCore.QModelIndex(), l, l)
-            print([item.pos() for item in self.items()])
-            self.graphicsView.fitInView(self.sceneRect(), QtCore.Qt.KeepAspectRatio)
+            # suppress drag of graphicsview when adding new node
+            event.accept()
+            return None
         return super().mousePressEvent(event)
         
         
-    
+class GrafferGraphicsView(QtWidgets.QGraphicsView):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    # def mouseMoveEvent(self, event):
+    #     print("qgraphicsview mouse move event", event)
+    #     super().mouseMoveEvent(event)
+
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -91,8 +103,13 @@ class MainWindow(QtWidgets.QMainWindow):
         self.graphicsView = self.findChild(QtWidgets.QGraphicsView, "graphicsView")
         self.scene = GraphScene(self.graphicsView, self.listView)
         self.graphicsView.setScene(self.scene)
-        self.graphicsView.setSceneRect(0, 0, 100, 100)
-        self.graphicsView.fitInView(self.scene.sceneRect(), QtCore.Qt.KeepAspectRatio)
+        # Make scene rectangle much larger, so drag outside of on-screen view works
+        self.graphicsView.setSceneRect(-1000, -1000, 2000, 2000)
+        # show a region smaller than the full scene rectangle
+        self.graphicsView.fitInView(-100, -100, 200, 200, QtCore.Qt.KeepAspectRatio)
+        # allow dragging of the on-screen view
+        self.graphicsView.setDragMode(QtWidgets.QGraphicsView.ScrollHandDrag)
+
         #self.graphicsView.setMouseTracking(True)
 
     def resizeEvent(self, *args):
